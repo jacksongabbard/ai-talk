e3a4f60200cbe356023a13ff91331ff105f46d1f
Jackson Gabbard <jg@jg.jgg>
2022-12-05 12:55:46 +0000

    Update the initial migration to include more useful tables

--- migrations/20221123165111-initial-db-setup.js
+++ migrations/20221123165111-initial-db-setup.js
@@ -10,7 +10,7 @@ module.exports = {
       id UUID DEFAULT uuid_generate_v4() NOT NULL PRIMARY KEY,
       created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
       updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-      team_name TEXT NULL,
+      display_name TEXT NULL,
       location TEXT NULL
     );
 
@@ -18,17 +18,74 @@ module.exports = {
       id UUID DEFAULT uuid_generate_v4() NOT NULL PRIMARY KEY,
       created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
       updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-      nickname TEXT NULL,
+      display_name TEXT NULL,
       location TEXT NULL,
-      team UUID NULL
+      team_id UUID NULL,
+      CONSTRAINT fk_team
+        FOREIGN KEY(team_id) 
+	        REFERENCES teams(id)
     );
+    
+    CREATE TABLE puzzles (
+      puzzle_id TEXT NOT NULL PRIMARY KEY,
+      created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+      updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+      display_name TEXT NULL,
+      description TEXT NULL
+    );
+
+    CREATE TABLE puzzle_instances (
+      id UUID DEFAULT uuid_generate_v4() NOT NULL PRIMARY KEY,
+      puzzle_id TEXT NOT NULL,
+      created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+      updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+      solved_at TIMESTAMP WITH TIME ZONE NULL,
+      puzzle_payload JSONB NULL,
+      solution_payload JSONB NULL,
+      CONSTRAINT fk_puzzle_id
+        FOREIGN KEY(puzzle_id) 
+	        REFERENCES puzzles(puzzle_id)
+    );
+
+    CREATE TABLE puzzle_instance_actions (
+      id UUID DEFAULT uuid_generate_v4() NOT NULL PRIMARY KEY,
+      puzzle_instance_id UUID NOT NULL,
+      team_id UUID NOT NULL,
+      created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+      updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+      payload JSONB NOT NULL,
+      CONSTRAINT fk_puzzle_instance_id
+        FOREIGN KEY(puzzle_instance_id) 
+	        REFERENCES puzzle_instances(id),
+      CONSTRAINT fk_team_id
+        FOREIGN KEY(team_id) 
+	        REFERENCES teams(id)
+    );
+
+    CREATE TABLE puzzle_instance_solution_attempts (
+      id UUID DEFAULT uuid_generate_v4() NOT NULL PRIMARY KEY,
+      puzzle_instance_id UUID NOT NULL,
+      user_id UUID NOT NULL,
+      solution_attempt_payload JSONB NOT NULL,
+      CONSTRAINT fk_puzzle_instance_id
+        FOREIGN KEY(puzzle_instance_id) 
+	        REFERENCES puzzle_instances(id),
+      CONSTRAINT fk_user_id
+        FOREIGN KEY(user_id) 
+	        REFERENCES users(id)
+    );
+
    `);
   },
 
   async down(queryInterface, Sequelize) {
     await queryInterface.sequelize.query(`
-    DROP TABLE teams;
     DROP TABLE users;
+    DROP TABLE teams;
+    DROP TABLE puzzle_instance_solution_attempts;
+    DROP TABLE puzzle_instance_actions;
+    DROP TABLE puzzle_instances;
+    DROP TABLE puzzles;
   `);
   },
 };
