2344f2df2bb41f3642f4c1c0ccfcbc818e7c966a
Jackson Gabbard <jg@jg.jgg>
2022-12-23 23:54:51 +0000

    Iterating on the UI

--- src/server/ui/nav/Nav.tsx
+++ src/server/ui/nav/Nav.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const Nav: React.FC = () => {
   return (
     <nav>
--- src/server/ui/profile/ProfileForm.tsx
+++ src/server/ui/profile/ProfileForm.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import { useCallback, useState } from 'react';
 
+import Button from '@mui/material/Button';
+import Paper from '@mui/material/Paper';
 import TextField from '@mui/material/TextField';
 
 import type User from 'src/lib/db/User';
@@ -9,10 +11,53 @@ type ProfileFormProps = {
 };
 
 const ProfileForm: React.FC<ProfileFormProps> = ({ user }) => {
+  const [editingProfile, _setEditingProfile] = useState(false);
+  const setEditingProfile = useCallback(() => {
+    _setEditingProfile(true);
+  }, [editingProfile]);
+
   return (
-    <div>
+    <div
+      css={{
+        maxWidth: '768px',
+        margin: '0 auto',
+      }}
+    >
+      <Paper square>
+        <div
+          css={{
+            padding: 'var(--spacing-massive)',
+          }}
+        >
+          <div className="phm pvl">
+            <TextField
+              value={user.userName}
+              label="User Name"
+              disabled={!editingProfile}
+              fullWidth={true}
+            />
+          </div>
+          <div className="phm pvl">
+            <TextField
+              value={user.location}
+              label="Location"
+              disabled={!editingProfile}
+              fullWidth={true}
+            />
+          </div>
+          <div>
+            {editingProfile ? (
+              <Button onClick={() => {}}>Save</Button>
+            ) : (
+              <Button variant="text" onClick={setEditingProfile}>
+                Edit profile
+              </Button>
+            )}
+          </div>
+        </div>
+      </Paper>
+      <br />
       <pre>{JSON.stringify(user, null, 4)}</pre>
-      <TextField value={user.userName} label="User Name" />
     </div>
   );
 };
