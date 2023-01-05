5a43655d840db86418d9998ea20c1501a9af1f68
Jackson Gabbard <jg@jg.jgg>
2023-01-05 14:51:37 +0000

    Update nodemon file path

--- scripts/build-dev.sh
+++ scripts/build-dev.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eu
 
-nodemon -e ts,tsx,css \
+./node_modules/.bin/nodemon -e ts,tsx,css \
   --watch src/ \
   --exec "./node_modules/.bin/esbuild src/server/index.ts \
       --bundle \
