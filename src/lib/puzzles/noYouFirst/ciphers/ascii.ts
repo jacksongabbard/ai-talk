c5513ffd709a43a8ae38bc85e78f5dcc0a67b8da
Jackson Gabbard <jg@jg.jgg>
2023-01-10 21:45:08 +0000

    Fix the error in NoYouFirst

--- src/lib/puzzles/noYouFirst/ciphers/ascii.ts
+++ src/lib/puzzles/noYouFirst/ciphers/ascii.ts
@@ -9,17 +9,13 @@ export function toAscii(input: string): string {
 export function fromAscii(input: string): string {
   const output = [];
   const parts = input.split(' ');
-  try {
-    for (const part of parts) {
-      const integerMaybe = parseInt(part, 10);
-      if (integerMaybe === undefined || integerMaybe < 10) {
-        output.push('!');
-      } else {
-        output.push(String.fromCharCode(integerMaybe));
-      }
+  for (const part of parts) {
+    const integerMaybe = parseInt(part, 10);
+    if (isNaN(integerMaybe)) {
+      output.push('NaN');
+    } else {
+      output.push(String.fromCharCode(integerMaybe));
     }
-  } catch (e) {
-    output.push('Error: That was definitely the wrong order.');
   }
   return output.join('');
 }
