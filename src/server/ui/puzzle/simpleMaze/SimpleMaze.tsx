6a8ec97f47cfc2cacb34c417607de7ccd3b8d89e
Jackson Gabbard <jg@jg.jgg>
2023-02-01 16:33:29 +0000

    Fix the right margin on the simple maze puzzle

--- src/server/ui/puzzle/simpleMaze/SimpleMaze.tsx
+++ src/server/ui/puzzle/simpleMaze/SimpleMaze.tsx
@@ -304,7 +304,7 @@ const SimpleMaze: React.FC<SimpleMazeProps> = ({
           borderBottom: '6px #383 solid',
           height: payload.maze.size * 100,
           width: payload.maze.size * 100,
-          marginRight: '376px',
+          marginRight: '416px',
         }}
       >
         {grid}
