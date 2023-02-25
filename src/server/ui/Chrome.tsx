a9dc9dbd0f9ad3168faf81ac60b1eba029bbcd7a
Jackson Gabbard <jg@jg.jgg>
2023-02-25 16:43:00 +0000

    Add OG metatags

--- src/server/ui/Chrome.tsx
+++ src/server/ui/Chrome.tsx
@@ -1,6 +1,14 @@
 import fs from 'fs';
 import path from 'path';
 import process from 'process';
+import getDotEnv from 'src/lib/dotenv';
+
+const env = getDotEnv();
+
+const host =
+  'https://' +
+  env.SERVER_HOST +
+  (env.SERVER_HOST.includes('local') ? ':' + env.SERVER_PORT : '');
 
 const css = fs
   .readFileSync(
@@ -19,6 +27,23 @@ const Chrome = ({ children, title }: ChromeProps): JSX.Element => {
       <head>
         <title>{title || 'Puzzles'}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={host} />
+        <meta
+          property="og:image"
+          content={`${host}/static/images/ohffs.io.png`}
+        />
+        <meta
+          property="og:image:alt"
+          content="Concentric circles of some unknowable purpose"
+        />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:height" content="1262" />
+        <meta property="og:image:width" content="1296" />
+        <meta
+          property="og:description"
+          content="Devilish puzzles to delight and infuratiate. Real-time, multiplayer."
+        />
       </head>
       <style type="text/css" dangerouslySetInnerHTML={{ __html: css }}></style>
       <body>{children}</body>
