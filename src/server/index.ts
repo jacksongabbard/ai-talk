977ded283eb3dba05c3a6f2501d987343cbc28ee
Jackson Gabbard <jg@jg.gg>
2023-10-18 16:54:29 +0100

    Fetch the indexes from the page_chunks table

--- src/server/index.ts
+++ src/server/index.ts
@@ -62,6 +62,7 @@ import { logEvent } from 'src/server/routes/api/events';
 import { storeChunk } from 'src/server/routes/api/storeChunk';
 import { resetIndex } from 'src/server/routes/api/resetIndex';
 import { search } from 'src/server/routes/api/search';
+import { getIndexes } from 'src/server/routes/api/getIndexes';
 
 const config = getDotEnv();
 
@@ -209,6 +210,7 @@ console.log('Bootstrapping the server...');
   router.post('/api/storeChunk', apiMiddleware, storeChunk);
   router.post('/api/resetIndex', apiMiddleware, resetIndex);
   router.post('/api/search', apiMiddleware, search);
+  router.post('/api/getIndexes', apiMiddleware, getIndexes);
 
   // Static resources
   router.get(staticResourcePathRegexp, staticResource);
